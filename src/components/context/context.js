b4978e227a81c866b349fcd876bb8eab2fd19d86
musaddekali <musaddekali.dev@gmail.com>
2022-04-08 10:10:30 +0600

    removed useState hook from context.js

--- src/components/context/context.js
+++ src/components/context/context.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer, useState } from "react";
+import React, { useContext, useEffect, useReducer} from "react";
 import { reducer } from "./reducer";
 import { PRODUCTS } from "../../FakeProduct/fakeProduct";
 
